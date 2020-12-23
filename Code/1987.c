f8886bbdaaded87c56ea286af21f27ccbbb72fbe
SeungGun <saverrw2864@naver.com>
2020-12-23 15:11:19 +0900

    Update 1987.c

--- Code/1987.c
+++ Code/1987.c
@@ -7,21 +7,18 @@ int max = -1;
 int dx[4] = { 0, 1, 0, -1 };
 int dy[4] = { 1, 0, -1, 0 };
 void backtracking(int row, int col, int count) {
-
+	if (row < 0 || row == R || col < 0 || col == C) {
+		return;
+	}
 	if (max < count)
 		max = count; 
 
 	for (int i = 0; i < 4; i++) {
-		int nxt_row = row + dx[i];
-		int nxt_col = col + dy[i];
-		if (nxt_row >= 0 && nxt_row < R && nxt_col >= 0 && nxt_col < C) {
-			if (!visited[board[row][col] - 'A']) {
-				visited[board[row][col] - 'A'] = 1;
-				backtracking(nxt_row, nxt_col, count + 1);
-				visited[board[row][col] - 'A'] = 0;
-			}
+		if (!visited[board[row][col] - 'A']) {
+			visited[board[row][col] - 'A'] = 1;
+			backtracking(row + dx[i], col + dy[i], count + 1);
+			visited[board[row][col] - 'A'] = 0;
 		}
-		
 	}
 	
 }
